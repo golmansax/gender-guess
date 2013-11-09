173abf9c11b80b552fd92163de64482e44919360
Holman Gao <holman@golmansax.com>
2013-11-09 15:50:01 -0800

    First commit with home controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GenderGuess::Application.config.secret_key_base = 'cb08fd7dc93e0b2b77fa80107cde61f47daf6feb90345d83c2204d8c5161b4135675762345d4ded07b44edc7a16770344c90e2db2a46e8db6db110b6ab0ecff9'
