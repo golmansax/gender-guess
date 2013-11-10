423ea47e871746fb0085ce017168bab48ef467d9
Holman Gao <holman@golmansax.com>
2013-11-10 09:45:56 -0800

    Adding person tests

--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class PersonTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "valid people" do
+    person = people(:holman)
+    assert person.save
+
+    person = people(:scarlett)
+    assert person.save
+  end
+
+  test "bogus gender" do
+    person = people(:bogus_gender)
+    assert !person.save
+  end
+
+  test "negative height" do
+    person = people(:negative_height)
+    assert !person.save
+  end
+
+  test "negative weight" do
+    person = people(:negative_weight)
+    assert !person.save
+  end
 end
