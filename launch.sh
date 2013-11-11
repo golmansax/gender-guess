8491c44e541bd1b249fd5af29e9363b9eff5eb82
Holman Gao <holman@golmansax.com>
2013-11-11 01:19:56 -0800

    Forcing tests to pass in order to launch prod server

--- launch.sh
+++ launch.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 #
 # @author holman
 #
@@ -8,12 +8,12 @@
 PROD_PORT=7701
 
 usage() {
-  echo "Usage ./launch.sh (dev|prod)"
+  echo 'Usage ./launch.sh (dev|prod)'
   exit
 }
 
 wrong_dir() {
-  echo "Error: run from project root dir"
+  echo 'Error: run from project root dir'
   exit
 }
 
@@ -28,8 +28,14 @@ if [ $1 == 'dev' ]; then
   rails server
 elif [ $1 == 'prod' ]; then
   # Migrate db
+  echo '*** Migrating db ***'
   rake db:migrate RAILS_ENV=production
 
+  # Make sure tests pass
+  echo '*** Making sure tests pass ***'
+  rake test
+
+  echo '*** Starting server ***'
   rails server -e production -p $PROD_PORT -d
 else
   usage
