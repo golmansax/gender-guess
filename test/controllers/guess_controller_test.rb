329f58511734e710f72f338050c78015a1bfc6bb
Holman Gao <holman@golmansax.com>
2013-11-11 00:57:42 -0800

    Fixing guess controller test

--- test/controllers/guess_controller_test.rb
+++ test/controllers/guess_controller_test.rb
@@ -3,8 +3,11 @@ require 'test_helper'
 class GuessControllerTest < ActionController::TestCase
   test "make guess" do
     person = people(:holman)
+    person_params = {
+      height: person.height, weight: person.weight, gender: person.gender
+    }
 
-    get :guess_parse, { person: person }
-    assert_redirected_to guess_path
+    get :guess_parse, { person: person_params }
+    assert_redirected_to guess_path(person.height, person.weight)
   end
 end
