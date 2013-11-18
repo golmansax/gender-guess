f9348de79d6c1c18fdfe893a4871af914954f3f3
Holman Gao <holman@golmansax.com>
2013-11-17 21:02:07 -0800

    Adding ability for cron job to create lin reg coeffs

---
+++ cron/README.md
@@ -0,0 +1,3 @@
+Two Variable Linear Regression
+===============
+Solves two variable linear regression in OCaml.
--- lib/gender.rb
+++ lib/gender.rb
@@ -1,4 +1,13 @@
 module Gender
+  puts '*** Initializing Gender module ***'
+
+  # We use the file generated from our daily cron job
+  # NOTE: if it is not there, then run the cron job to generate it
+  lines = File.readlines('cron/generated-lin_reg_coeffs.txt')
+  @a = lines[0].to_f
+  @b1 = lines[1].to_f
+  @b2 = lines[2].to_f
+
   # Let's store in here the function we use to calculate gender
   #   @param(height) - should be a number in inches
   #   @param(weight) - should be a number in pounds
@@ -6,7 +15,7 @@ module Gender
     # Manually calculated linear regression model
     # - > 0 is male, < 0 is female
     # - Simulated with male = 1, female = -1
-    val = -20.25 + 0.3494 * height + -0.0241 * weight
+    val = @a + @b1 * height + @b2 * weight
 
     return val > 0 ? 'M' : 'F'
   end
